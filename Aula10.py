e447102e19c96e0bff3c6f722f108fdccc11b38f
Alexandr3S <vitor.black.hero@gmail.com>
2025-01-24 20:38:14 -0300

    Add files via upload

---
+++ Aula10.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+print ("\n ----- OBTENDO DADOS -----")
+
+endereco_dados = 'https://www.ispdados.rj.gov.br/Arquivos/BaseDPEvolucaoMensalCisp.csv'
+
+## Criando o DataFrame ocorrencias
